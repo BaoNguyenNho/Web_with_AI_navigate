08f3ff9a1ee5f88418b095bd781445650307caf9
Bao Ng <89015828+BaoNguyenNho@users.noreply.github.com>
2022-12-27 02:09:52 +0700

    Update app.py

--- AI/app.py
+++ AI/app.py
@@ -1,18 +1,31 @@
-from flask import Flask, request, redirect
-from flask_restful import Resource, Api
-from json import dumps
-from flask import jsonify
+# from flask import Flask, request, redirect
+# from flask_restful import Resource, Api
+# from json import dumps
+# from flask import jsonify
 
-app=Flask(__name__)
-api=Api()
-class Send(Resource):
-    def post(self):
-        # query = conn.execute("select * from employees") # Dòng này thực hiện truy vấn và trả về json
-        res = {'employees': 'Hello'} 
-        return jsonify(res)# Tìm và thêm cột đầu tiên là Employee ID
+# app=Flask(__name__)
+# api=Api()
+# class Send(Resource):
+#     def post(self):
+#         # query = conn.execute("select * from employees") # Dòng này thực hiện truy vấn và trả về json
+#         res = {'employees': 'Hello'} 
+#         return jsonify(res)# Tìm và thêm cột đầu tiên là Employee ID
 
 
-api.add_resource(Send, '/predict')
-api.init_app(app)
-if __name__=='__main__':
-    app.run(debug=True)
+# api.add_resource(Send, '/predict')
+# api.init_app(app)
+# if __name__=='__main__':
+#     app.run(debug=True)
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About'
